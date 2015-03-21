751ba327216bd488396c91dc73817d8695fe04a1
Harrison Ianatchkov <harrison1993@gmail.com>
2015-03-20 22:40:40 -0400

    should work now

---
+++ efsm.pl
@@ -0,0 +1,57 @@
+%% Top level states
+state(exit).
+state(init).
+state(idle).
+state(monitoring).
+state(error_diagnosis).
+state(safe_shutdown).
+
+%% States under init state
+state(boot_hw).
+state(senchk).
+state(tchk).
+state(psichk).
+state(ready).
+
+%% States under monitor state
+state(monidle).
+state(regulate_environment).
+state(lockdown).
+
+%% State under error_diagnosis state
+state(error_rcv).
+state(applicable_rescue).
+state(reset_module_data).
+
+%% Initial states
+initial_state(dormant, null).
+initial_state(boot_hw, init).
+initial_state(monidle, monitor).
+initial_state(error_rcv, error_diagnosis).
+
+%% Superstates
+superstate(init, ).
+superstate(init, ).
+superstate(init, ).
+superstate(init, ).
+superstate(init, ).
+superstate(monitor, ).
+superstate(monitor, ).
+superstate(monitor, ).
+superstate(error_diagnosis, ).
+superstate(error_diagnosis, ).
+superstate(error_diagnosis, ).
+
+%% transition(source, destination, event, guard, action).
+
+%% Transitions within top level
+transition(dormant, exit, kill, null, null).
+transition(dormant, init, start, null, 'load drivers').
+transition(init, idle, init_ok, null, null).
+transition(init, error_diagnosis, init_crash, null, 'broadcast init_err_msg').
+transition(idle, monitoring, begin_monitoring, null, null).
+transition(idle, error_diagnosis, idle_crash, null, 'broadcast idle_err_msg').
+transition(monitoring, error_diagnosis, monitor_crash, not(inlockdown), 'broadcast idle_err_msg').
+transition(error_diagnosis, init, retry_init, 'retry < 3', 'retry++').
+transition(error_diagnosis, idle, idle_rescue, null, null).
+transition(error_diagnosis, monitoring, null, null, null).
