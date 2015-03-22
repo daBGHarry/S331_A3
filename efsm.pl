c6673feb203bd3f14b489c96228eca42a0ee4a78
justin-yip <justin.n.yip@gmail.com>
2015-03-22 18:24:34 -0400

    Finished most of the odd rules. Need to be revised

--- efsm.pl
+++ efsm.pl
@@ -109,28 +109,40 @@ transition(reset_module_data, exit, reset_to_stable, null, null).
 %% =============================================================================
 
 %% 1. is loop(Event, Guard) succeeds by finding a loop edge. We assume that an edge can be represented by a non-null event-guard pair.
-is_loop(Event, Guard):- transition(X, X, Event, Guard, _).
+is_loop(Event,Guard):- transition(X,X,Event,Guard,_).
 
 %% 2. 
 all_loops(Set):- findall(Edge,
 
 %% 3. is edge(Event, Guard) succeeds by finding an edge.
-is_edge(Event, Guard):- transition(_, _, Event, Guard, _).
+is_edge(Event,Guard):- transition(_,_,Event,Guard,_).
 
 %% 5. is link(Event, Guard) succeeds by finding a link edge.
+is_link(Event,Guard) :- 															%% ** WTF is a link edge????????
 
-%% 7. ancestor(Ancestor, Descendant) is a utility rule that succeeds by returning an ancestor to a given state.
+%% 7. ancestor(Ancestor,Descendant) is a utility rule that succeeds by returning an ancestor to a given state. 	%% ** are we trying to prove that descendant has ancestor? or find ancestor to descendant?????
+ancestor(Ancestor, Descendant) :- transition(Ancestor, Descendant,_,_,_). %% returns the ancestor to the state Descendant
+%% ** ancestor(Ancestor, Descendant) :- transition(Ancestor,Descendant,_,_,_).
+%% ** ancestor(Ancestor, Descendant) :- transition(Ancestor,X,_,_,_), ancestor(X, Descendant).
 
 %% 9. all states(L) succeeds by returning a list of all states.
+states(L) :- findall(X,state(X),L). %% returns a list of all states in EFSM
 
 %% 11. get starting state(State) succeeds by returning the top-level starting state.
+get_starting_state(State) :- initial_state(State,null).	%% if state is initial state
+get_starting_state(State) :- superstate(X,State), get_starting_state(X). %% if state is in lower level states, find the intial state of its superstate
+get_starting_state(State) :- get_starting_state(ancestor(Ancestor, State)). %% if state has ancestor, see if the ancestor is the initial state
 
-%% 13. graph is reflexive succeeds if the entire EFSM is reflexive.
+%% 13. graph is reflexive succeeds if the entire EFSM is reflexive.	%% ** Every state is reflexive???
+graph_is_reflexive :- list_is_reflexive(states(L)).
+list_is_reflexive([H]) :- transition(H,H,_,_,_).	%% base case
+list_is_reflexive([H|T]) :- transition(H,H,_,_,_), list_is_reflexive(T). %% iterates through list of all states and checks if they are all reflexive
 
 %% 15. get events(Ret) succeeds by returning a set of all events.
+get_events(Ret) :- findall(Event, transition(_,_,Event,_,_),L),list_to_set(L,Ret).
 
 %% 17. get only guarded(Ret) succeeds by returning state pairs that are associated by guards only.
- 
+get_only_guarded(Ret) :- findall([Ancestor,Descendant|[]],transition(Ancestor,Descendant,_,null,null),Ret).
 
 
 
